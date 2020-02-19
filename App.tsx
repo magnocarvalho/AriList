c9311f44f66fe1aba076a446729c0389386d8204
Magno Carvalho <magnocarv@hotmail.com>
2020-02-19 19:19:31 -0300

    nem splash

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const [loadFont, setLoadFont] = useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
+    }).then(() => {
+      setLoadFont(true);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open text up App.tsx to start working on your app!</Text>
-    </View>
+    loadFont && (
+      <View style={styles.container}>
+        <Text>Start</Text>
+      </View>
+    )
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
